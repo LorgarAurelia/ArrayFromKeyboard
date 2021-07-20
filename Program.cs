f4f76b18161a06a9152993789bd59981e3b76c66
David Maltsev <luterfaeron@gmail.com>
2021-07-20 18:35:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayFromKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfInputData;
+            arrayOfInputData = new int[3];
+
+            for (int i = 0; i < arrayOfInputData.Length; i++)
+            {
+                arrayOfInputData[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(arrayOfInputData[1]);
+            Console.ReadLine();
+        }
+    }
+}
